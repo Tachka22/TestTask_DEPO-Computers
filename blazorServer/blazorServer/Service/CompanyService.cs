dc951b22604535a1536c6124be09bfa9ea070fc1
Vlad <chinnov.vlad@gmail.com>
2024-04-19 12:32:13 +0300

    Сервисы работы с БД

---
+++ blazorServer/blazorServer/Service/CompanyService.cs
@@ -0,0 +1,59 @@
+﻿using blazorServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazorServer.Service
+{
+    /// <summary>
+    /// Сервисы работы с таблицей "Company"
+    /// </summary>
+    public class CompanyService
+    {
+        private readonly AppDbContext _context;
+        public CompanyService(AppDbContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Добавить одну компанию.
+        /// </summary>
+        /// <param name="company"></param>
+        /// <returns></returns>
+        public async Task<bool> CreateAsync(Company company)
+        {
+            //Если добавляемая компания уже есть в БД то она НЕ будет повторно добавлена.
+            var findCompany = _context.Companies.FirstOrDefaultAsync(x => x.Inn == company.Inn.ToString());
+
+            if (findCompany == null)
+                await _context.Companies.AddAsync(company);
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        /// <summary>
+        /// Добавить несколько компаний.
+        /// </summary>
+        /// <param name="companies"></param>
+        /// <returns></returns>
+        public async Task<bool> CreateRangeAsync(IList<Company> companies)
+        {
+            //Если добавляемая компания уже есть в БД то она НЕ будет повторно добавлена.
+            foreach (var item in companies)
+            {
+                var company = _context.Companies.FirstOrDefaultAsync(x => x.Inn == item.Inn.ToString());
+                if (company == null)
+                    await _context.Companies.AddAsync(item);
+            }
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+        /// <summary>
+        /// Получить список компаний.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Company>> GetAllAsync()
+        {
+            return await _context.Companies.ToListAsync();
+        }
+    }
+}
---
+++ blazorServer/blazorServer/Service/EmployeeService.cs
@@ -0,0 +1,60 @@
+﻿using blazorServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace blazorServer.Service
+{
+    /// <summary>
+    /// Сервисы работы с таблицей "Сотрудники"
+    /// </summary>
+    public class EmployeeService
+    {
+        private readonly AppDbContext _context;
+        public EmployeeService(AppDbContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Создать одного сотрудника.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public async Task<bool> CreateAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        /// <summary>
+        /// Вывести всех сотрудников.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Employee>> GetAllAsync()
+        {
+            return await _context.Employees.Include(u => u.Company).ToListAsync();
+        }
+        /// <summary>
+        /// Создать несколько сотрудников.
+        /// </summary>
+        /// <param name="employees"></param>
+        /// <returns></returns>
+        public async Task<bool> CreateRangeAsync(List<Employee> employees)
+        {
+            foreach (var employee in employees)
+            {
+                var com = await _context.Companies.FirstOrDefaultAsync(x => x.Name == employee.Company.Name.ToString());
+                var emp = await _context.Employees.FirstOrDefaultAsync(x => x.PassportNumber == employee.PassportNumber.ToString());
+                //проверка существования компании в которую требуется занести сотрудника
+                //проверка отсутсвия нового сотрудника(по паспорту) в БД
+                if (com != null && (emp == null ||
+                    emp!.PassportSeries != employee.PassportSeries))
+                {
+                    employee.Company = com;
+                    await _context.Employees.AddAsync(employee);
+                }
+            }
+            await _context.SaveChangesAsync();
+            return true;
+
+        }
+    }
+}
